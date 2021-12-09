3f1a8b4f85e0e0520051dd686b7396d03320b90b
NamrataRaikwar2002 <92174796+NamrataRaikwar2002@users.noreply.github.com>
2021-12-09 15:32:43 +0530

    fix:task1

---
+++ task1.js
@@ -0,0 +1,40 @@
+var user=require("readline-sync");
+const chalk = require("chalk");
+console.log(chalk.blue('Hello world!'));
+var name=user.question(chalk.red("what's your name? "));
+console.log(chalk.blue("welcome "+name+" Do you know Namrata? "));
+var score=0;
+var totalscore=10
+
+function game(question,answer){
+            var q=user.question(chalk.black(question))
+            if (q.toUpperCase()===answer.toUpperCase()){
+                    score=score+2;
+                    console.log(" Great! your score is",score+".");
+            }
+            else{
+                console.log("wrong!!");
+            
+            }
+        }
+ 
+var ques=[{question:"where is she from?",answer:"Delhi"},{question:"what is she doing nowadays?",answer:"Coding course"},{question:"what is her  age?",answer:"19"},{question:"what is her favourite colour?",answer:"Black"},{question:"Do she a NAVODAYAN?",answer:"yes"}]
+
+for (i of ques){
+    game(i.question,i.answer)
+}
+if (score>=10){
+    console.log(chalk.green("wow!you know all about Namrata!"))
+}
+
+else{
+    console.log(chalk.red("game over! your score is",score))
+}
+
+var highScore=[{name:"Bhoomi",gameScore:'8'},{name:"Neetu",gameScore:"6"}]
+
+for(var a of highScore){
+    console.log(a.name+"'s score is", a.gameScore);
+
+}
+console.log(chalk.cyan("if you have beaten the score send me screenshot"));
